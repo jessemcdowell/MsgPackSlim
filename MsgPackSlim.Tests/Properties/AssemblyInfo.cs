4affdbc40b21102cf997ff2648b798e614ff0455
jessemcdowell <jessemcd1@gmail.com>
2015-05-30 18:09:23 -0700

    set up empty projects

---
+++ MsgPackSlim.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MsgPackSlim Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MsgPackSlim.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2015 Jesse McDowell")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
